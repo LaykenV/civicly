5d558cf2bb578db75a0549f710edd73278871f79
Layken Varholdt <laykenv@gmail.com>
2025-08-11 19:36:14 -0500

    simpler approach

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,10 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+  ],
 };
 
 export default function RootLayout({
